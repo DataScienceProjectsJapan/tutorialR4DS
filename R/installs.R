1c61d25ac0052b1e83dcd20012a92b5573e5def4
William Claster <william108@gmail.com>
2021-08-21 21:16:46 +0900

    ni ### Intermediate 5.2.4 DN

---
+++ R/installs.R
@@ -0,0 +1,12 @@
+install.packages("remotes")
+remotes::install_github("rstudio-education/gradethis")
+
+install.packages(c("devtools", "roxygen2", "learnr"))
+library(devtools)
+library(usethis)
+library(roxygen2)
+library(learnr)
+
+# install.packages("remotes") # require {remotes}
+
+remotes::install_github("rstudio/gradethis")
