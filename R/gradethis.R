66e237e6d90ef8ace4304b669683ae7b703fed0f
William Claster <william108@gmail.com>
2021-08-27 13:37:24 +0900

    ### 5.3.1 Challenging  YY

---
+++ R/gradethis.R
@@ -0,0 +1,3 @@
+```{r delay-check}
+gradethis::grade_this_code(correct = "Good work! Shortly we will learn an alternative way to construct a new variable with the mutate() function", incorrect = "Not quite. {code_feedback()} {random_encouragement()}")
+```
